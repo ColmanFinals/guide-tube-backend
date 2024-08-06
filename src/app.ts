78b5a6436fceebd192ae3ffb7427e57eb2c43992
Bar Agmon <48044742+BarAgmon@users.noreply.github.com>
2024-08-06 23:05:38 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // src/app.ts
 import fs from 'fs'
-import https from 'https'
+import https from 'https';
+import http from 'http';
 import express from 'express';
 import {closeDB, connectDB} from './db/db';
 import authRouter from './routes/authRoutes';
